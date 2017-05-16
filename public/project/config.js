f9bc903ee8e99eb428442e01a2129e1fe894815b
Srijit Ravishankar <Srijit@Srijits-MacBook-Air.local>
2017-05-16 17:29:11 -0400

    refactored

---
+++ public/project/config.js
@@ -0,0 +1,59 @@
+$(document).click(function(e) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768){
+        if (!$(e.target).is('a') && !$(e.target).is('input'))  {
+            $('#collapsable-nav').collapse('hide');
+        }
+    }
+});
+
+
+
+(function () {
+    angular
+        .module('BonAppetit',['ui.router'])
+        .config(configuration);
+
+            function configuration($stateProvider, $urlRouterProvider,$locationProvider) {
+
+            $locationProvider.hashPrefix('');
+
+            $stateProvider
+
+            //route for the main page
+                .state('index',{
+                    url:'/',
+                    views: {
+                        'header': {
+                            templateUrl: 'views/main/header.view.html'
+                        },
+                        'content': {
+                            templateUrl: 'views/main/main.view.html'
+                        }
+                    }
+                })
+
+                //route for register page
+                .state('index.register', {
+                    url:'register',
+                    views: {
+                        'content@': {
+                            templateUrl : 'views/users/register.view.html'
+                        }
+                    }
+                })
+
+                //route for login page
+                .state('index.login', {
+                    url:'login',
+                    views: {
+                        'content@': {
+                            templateUrl : 'views/users/login.view.html'
+                        }
+                    }
+                });
+
+            $urlRouterProvider.otherwise('/');
+
+        }
+})();

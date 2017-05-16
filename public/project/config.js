962f9a1f965f5a02556eb69d65e2ed17aea2144a
Srijit Ravishankar <Srijit@Srijits-MacBook-Air.local>
2017-05-16 17:50:10 -0400

    refactored

--- public/project/config.js
+++ public/project/config.js
@@ -1,13 +1,9 @@
 $(document).click(function(e) {
     var screenWidth = window.innerWidth;
     if (screenWidth < 768){
-        if (!$(e.target).is('a') && !$(e.target).is('input'))  {
             $('#collapsable-nav').collapse('hide');
-        }
     }
 });
-
-
 
 (function () {
     angular

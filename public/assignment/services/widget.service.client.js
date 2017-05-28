8a874ea9cd315a23ee762033889d34b34ed81584
Srijit Ravishankar <Srijit@Srijits-MacBook-Air.local>
2017-05-27 22:05:37 -0400

    started with widgets

--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -11,7 +11,7 @@
             { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%", "url": "http://lorempixel.com/400/200/"},
             { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": '<p>It’s possible that we’re getting better at addressing the problems that are facing bee populations. VanEnglesdorp, a University of Maryland entomologist, believes that a new product for fighting the <a href="http://gizmodo.com/a-deadly-bee-virus-is-spreading-and-only-humans-can-sto-1756705772" rel="nofollow">mite </a><a href="http://gizmodo.com/a-deadly-bee-virus-is-spreading-and-only-humans-can-sto-1756705772" rel="nofollow"><em>Varroa destructor</em></a><em> </em>is one of the primary reasons for the more encouraging numbers this year. The mites have been a key factor in what’s commonly referred to as “colony collapse disorder” because they carry the <a href="https://en.wikipedia.org/wiki/Deformed_wing_virus" target="_blank" rel="noopener">deformed wing virus</a>.</p>'},
             { "_id": "567", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-            { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%", "url": "https://youtu.be/AM2Ivdi9c4E" },
+            { "_id": "678", "widgetType": "YOUTUBE", "pageId": "432", "width": "100%", "url": "https://youtu.be/AM2Ivdi9c4E" },
             { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
         ];
 
